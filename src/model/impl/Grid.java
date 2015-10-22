a38c72c18ece7c4cbf6beceebfbac7b299f04059
xBeat3r <mathmatze@googlemail.com>
2015-10-22 17:19:41 +0200

    added basic Grid

--- src/model/impl/Grid.java
+++ src/model/impl/Grid.java
@@ -1,5 +1,15 @@
 package model.impl;
 
 public class Grid {
-
+    private Cell[][] cells;
+    
+    public Grid(int height, int width) {
+        cells = new Cell[height][width];
+        
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                cells[row][col] = new Cell(row, col);
+            }
+        }
+    }
 }
