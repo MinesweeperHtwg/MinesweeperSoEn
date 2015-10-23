8537bcf9d4a5bac010a9d2c06d6e43e5dac84b8b
Nibuchma <nico.buchmann@web.de>
2015-10-23 11:37:28 +0200

    extended initialization of grid

--- src/model/impl/Grid.java
+++ src/model/impl/Grid.java
@@ -5,10 +5,19 @@ public class Grid {
     
     public Grid(int height, int width) {
         cells = new Cell[height][width];
-        
+
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
                 cells[row][col] = new Cell(row, col);
+            }
+        }
+
+        for (Cell[] rows: cells) {
+            for (Cell cell: rows) {
+                cell.setFlag(false);
+                cell.setOpened(false);
+                cell.setContainsMine(false);
+                cell.setMines(0);
             }
         }
     }
