9b5f8de5fe3d015caccf0334f6eacb1c82de3679
xBeat3r <mathmatze@googlemail.com>
2015-10-23 13:09:35 +0200

    added alternative factory

---
+++ src/minesweeper/model/impl/DebugGridFactory.java
@@ -0,0 +1,13 @@
+package minesweeper.model.impl;
+
+import minesweeper.model.IGridFactory;
+
+public class DebugGridFactory implements IGridFactory {
+
+    @Override
+    public Grid getGrid() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
