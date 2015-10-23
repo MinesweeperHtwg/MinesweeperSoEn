2ce2e0547c71b5db6beb7b2e9b741e8994fa7ee8
xBeat3r <mathmatze@googlemail.com>
2015-10-23 13:08:38 +0200

    added grid generation

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,7 +1,17 @@
 package minesweeper;
 
+import minesweeper.model.IGridFactory;
+import minesweeper.model.impl.DebugGridFactory;
+import minesweeper.model.impl.Grid;
+
 public class Minesweeper {
     public static void main(String[] args) {
         System.out.println("I'm alive!");
+        
+        IGridFactory gFact = new DebugGridFactory();
+        
+        Grid grid = gFact.getGrid();
+        
+        grid.getMines(0, 0);
     }
 }
