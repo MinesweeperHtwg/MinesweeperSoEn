13dcb52852582cdc6a936259ae5735f81e78bc35
Matze <mathmatze@googlemail.com>
2015-10-27 00:52:48 +0100

    added comment

--- src/minesweeper/model/IGridFactory.java
+++ src/minesweeper/model/IGridFactory.java
@@ -4,6 +4,10 @@ import minesweeper.model.impl.Grid;
 
 public interface IGridFactory {
     
+	/**
+	 * Returns a generated grid
+	 * @return a generated grid
+	 */
     public abstract Grid getGrid();
 
 }
