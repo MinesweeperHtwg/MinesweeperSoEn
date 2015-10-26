31219260ed8bbd1efb217aec576fb044e8ffa877
Matze <mathmatze@googlemail.com>
2015-10-27 00:36:28 +0100

    GridFactory places mines randomly on the field

--- src/minesweeper/model/impl/GridFactory.java
+++ src/minesweeper/model/impl/GridFactory.java
@@ -1,44 +1,51 @@
 package minesweeper.model.impl;
 
+import java.util.Collections;
+import java.util.List;
+
 import minesweeper.model.IGridFactory;
 
 public class GridFactory implements IGridFactory {
-    private int height;
-    private int width;
-    private int mines;
-    
-    public GridFactory(int height, int width, int mines) {
-        this.height = height;
-        this.width = width;
-        this.mines = mines;
-    }
-    
-    /* (non-Javadoc)
-     * @see minesweeper.model.impl.IGridFactory#getGrid()
-     */
-    @Override
-    public Grid getGrid() {
-        Cell[][] cells = new Cell[height][width];
+	private int height;
+	private int width;
+	private int mines;
 
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                cells[row][col] = new Cell(row, col);
-            }
-        }
+	public GridFactory(int height, int width, int mines) {
+		if (height * width > mines) {
+			throw new IllegalArgumentException(
+					"Cant construct a grid with more mines than cells!");
+		}
 
-        for (Cell[] rows: cells) {
-            for (Cell cell: rows) {
-                cell.setFlag(false);
-                cell.setOpened(false);
-                cell.setContainsMine(false);
-                cell.setMines(0);
-            }
-        }
-        
-        Grid grid = new Grid(cells);
-        
-        return grid;
-    }
-    
-    
+		this.height = height;
+		this.width = width;
+		this.mines = mines;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see minesweeper.model.impl.IGridFactory#getGrid()
+	 */
+	@Override
+	public Grid getGrid() {
+		Cell[][] cells = new Cell[height][width];
+
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				cells[row][col] = new Cell(row, col);
+			}
+		}
+
+		Grid grid = new Grid(cells);
+
+		// Distribute mines on the grid
+		List<Cell> cellList = grid.getList();
+		Collections.shuffle(cellList);
+		for (int i = 0; i < mines; i++) {
+			cellList.get(i).setContainsMine(true);
+		}
+
+		return grid;
+	}
+
 }
