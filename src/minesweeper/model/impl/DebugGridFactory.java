549d78cc621b39c59796165bd91286cde5ddb562
Matze <mathmatze@googlemail.com>
2015-10-27 00:47:10 +0100

    implemented DebugGridFactory

--- src/minesweeper/model/impl/DebugGridFactory.java
+++ src/minesweeper/model/impl/DebugGridFactory.java
@@ -1,13 +1,28 @@
 package minesweeper.model.impl;
 
+import java.util.List;
+
 import minesweeper.model.IGridFactory;
 
 public class DebugGridFactory implements IGridFactory {
+	private Grid grid;
 
-    @Override
-    public Grid getGrid() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public DebugGridFactory(Grid grid) {
+		this.grid = grid;
+	}
+
+	@Override
+	public Grid getGrid() {
+		List<Cell> cellList = grid.getList();
+		for (Cell cell : cellList) {
+			if (Math.random() < 0.75)
+				cell.setOpened(true);
+			if (Math.random() < 0.1)
+				cell.setFlag(true);
+
+			cell.setMines((int) (Math.random() * 4));
+		}
+		return grid;
+	}
 
 }
