903868bf834a1e0c2fdd9e046bcc8a2cd718bdf6
Matze <mathmatze@googlemail.com>
2015-10-27 00:53:28 +0100

    application now prints a random grid

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,17 +1,16 @@
 package minesweeper;
 
 import minesweeper.model.IGridFactory;
-import minesweeper.model.impl.DebugGridFactory;
+import minesweeper.model.impl.RandomGridFactory;
 import minesweeper.model.impl.Grid;
+import minesweeper.model.impl.GridFactory;
 
 public class Minesweeper {
-    public static void main(String[] args) {
-        System.out.println("I'm alive!");
-        
-        IGridFactory gFact = new DebugGridFactory();
-        
-        Grid grid = gFact.getGrid();
-        
-        grid.getMines(0, 0);
-    }
+	public static void main(String[] args) {
+		IGridFactory gFact = new GridFactory(10, 10, 20);
+		Grid grid = gFact.getGrid();
+		grid = new RandomGridFactory(grid).getGrid();
+
+		System.out.println(grid.mkString());
+	}
 }
