a5d5b565108df8426e535df2f5c716254066fb5a
Matze <mathmatze@googlemail.com>
2015-10-27 19:33:52 +0100

    test Cell char constructor and equals

--- test/minesweeper/model/impl/CellTest.java
+++ test/minesweeper/model/impl/CellTest.java
@@ -6,14 +6,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CellTest {
-	
+
 	Cell cell;
 
 	@Before
 	public void setUp() throws Exception {
-		cell = new Cell(1,2);
+		cell = new Cell(1, 2);
 	}
-	
+
+	@Test
+	public void testCellChar() {
+		assertTrue(new Cell(1, 2, 'M').isMine());
+		assertFalse(new Cell(1, 2, ' ').isOpened());
+		assertTrue(new Cell(1, 2, 'F').isFlag());
+		for (int i = 0; i < 9; i++) {
+			assertEquals(i,
+					new Cell(1, 2, Character.forDigit(i, 10)).getMines());
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCellCharIllegal() {
+		new Cell(1,2,'9');
+	}
+
 	@Test
 	public void testCol() {
 		assertEquals(1, cell.getCol());
@@ -23,7 +39,7 @@ public class CellTest {
 	public void testRow() {
 		assertEquals(2, cell.getRow());
 	}
-	
+
 	@Test
 	public void testGetMines() {
 		cell.setMines(1);
@@ -31,31 +47,31 @@ public class CellTest {
 		cell.setMines(0);
 		assertEquals(0, cell.getMines());
 	}
-	
+
 	@Test
 	public void testIsFlag() {
 		cell.setFlag(true);
-		assertEquals(true, cell.isFlag());
+		assertTrue(cell.isFlag());
 		cell.setFlag(false);
-		assertEquals(false, cell.isFlag());
+		assertFalse(cell.isFlag());
 	}
-	
+
 	@Test
 	public void testContainsMine() {
 		cell.setIsMine(true);
-		assertEquals(true, cell.isMine());
+		assertTrue(cell.isMine());
 		cell.setIsMine(false);
-		assertEquals(false, cell.isMine());
+		assertFalse(cell.isMine());
 	}
-	
+
 	@Test
 	public void testIsOpened() {
 		cell.setOpened(true);
-		assertEquals(true, cell.isOpened());
+		assertTrue(cell.isOpened());
 		cell.setOpened(false);
-		assertEquals(false, cell.isOpened());
+		assertFalse(cell.isOpened());
 	}
-	
+
 	@Test
 	public void testMkString() {
 		assertEquals(" ", cell.mkString());
@@ -74,5 +90,30 @@ public class CellTest {
 		assertEquals("1", cell.mkString());
 		cell.setMines(0);
 		assertEquals("0", cell.mkString());
+	}
+	
+	@Test
+	public void testEquals() {
+		Cell c1 = new Cell(1,2,'2');
+		assertEquals(c1, c1);
+		assertFalse(c1.equals(null));
+		assertFalse(c1.equals(1));
+		
+		Cell c2 = new Cell(2,2,'2');
+		assertFalse(c1.equals(c2));
+		c2 = new Cell(1,1,'2');
+		assertFalse(c1.equals(c2));
+		c2 = new Cell(1,2,'1');
+		assertFalse(c1.equals(c2));
+		c2 = new Cell(1,2,'M');
+		assertFalse(c1.equals(c2));
+		c2 = new Cell(1,2,'F');
+		assertFalse(c1.equals(c2));
+		c2 = new Cell(1,2,' ');
+		c2.setMines(2);
+		assertFalse(c1.equals(c2));
+
+		c2 = new Cell(1,2,'2');
+		assertEquals(c1, c2);
 	}
 }
