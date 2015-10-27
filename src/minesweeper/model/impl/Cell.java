213854b316fbd9986de4d22caaaa9c3193813c3b
Matze <mathmatze@googlemail.com>
2015-10-27 19:05:18 +0100

    added convenience constructor and equals method

--- src/minesweeper/model/impl/Cell.java
+++ src/minesweeper/model/impl/Cell.java
@@ -14,20 +14,55 @@ public class Cell implements ICell {
 		this(col, row, 0, false, false, false);
 	}
 
-	public Cell(int col, int row, int mines, boolean opened,
-			boolean containsMine, boolean flag) {
+	public Cell(int col, int row, char type) {
+		this.col = col;
+		this.row = row;
+		if (type == 'F') {
+			setFlag(true);
+			setIsMine(false);
+			setOpened(false);
+			setMines(0);
+			return;
+		}
+		if (type == 'M') {
+			setFlag(false);
+			setIsMine(true);
+			setOpened(true);
+			setMines(0);
+			return;
+		}
+		if (type == ' ') {
+			setFlag(false);
+			setIsMine(false);
+			setOpened(false);
+			setMines(0);
+			return;
+		}
+
+		int mines = Character.getNumericValue(type);
+		if (mines < 0 || mines > 9) {
+			throw new IllegalArgumentException("Illegal Cell type");
+		}
+		setMines(mines);
+		setFlag(false);
+		setIsMine(false);
+		setOpened(true);
+	}
+
+	public Cell(int col, int row, int mines, boolean opened, boolean isMine,
+			boolean flag) {
 		this.col = col;
 		this.row = row;
 		this.mines = mines;
 		this.opened = opened;
-		this.isMine = containsMine;
+		this.isMine = isMine;
 		this.flag = flag;
 	}
 
 	public int getCol() {
 		return col;
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
@@ -39,7 +74,7 @@ public class Cell implements ICell {
 	public boolean isFlag() {
 		return flag;
 	}
-	
+
 	public boolean isMine() {
 		return isMine;
 	}
@@ -73,5 +108,29 @@ public class Cell implements ICell {
 			return "M";
 		}
 		return String.valueOf(mines);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cell other = (Cell) obj;
+		if (col != other.col)
+			return false;
+		if (flag != other.flag)
+			return false;
+		if (isMine != other.isMine)
+			return false;
+		if (mines != other.mines)
+			return false;
+		if (opened != other.opened)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
 	}
 }
