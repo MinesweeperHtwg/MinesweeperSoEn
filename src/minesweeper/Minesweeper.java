464730bf04b7514f433fbbeef9713a8ca3e7e57b
Matze <mathmatze@googlemail.com>
2015-11-05 22:02:17 +0100

    Minesweeper now prints a completly open board which has correct contents

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,15 +1,18 @@
 package minesweeper;
 
 import minesweeper.model.IGridFactory;
-import minesweeper.model.impl.RandomGridFactory;
+import minesweeper.model.impl.Cell;
 import minesweeper.model.impl.Grid;
 import minesweeper.model.impl.GridFactory;
+import minesweeper.model.impl.Cell.State;
 
 public class Minesweeper {
 	public static void main(String[] args) {
-		IGridFactory gFact = new GridFactory(10, 20, 20);
+		IGridFactory gFact = new GridFactory(10, 20, 50);
 		Grid grid = gFact.getGrid();
-		grid = new RandomGridFactory(grid).getGrid();
+		for (Cell cell : grid.getCells()) {
+			cell.setState(State.OPENED);
+		}
 
 		System.out.println(grid.mkString());
 	}
